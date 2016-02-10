7938bf105ae56815da21206a44540c50f1f662f8
Sam Copeland <samcopeland7@gmail.com>
2016-02-10 10:59:38 -0500

    added datetimepicker functionality and cleaned up view pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210134035) do
+ActiveRecord::Schema.define(version: 20160210154449) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20160210134035) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
-    t.datetime "deadline"
+    t.string   "deadline"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
